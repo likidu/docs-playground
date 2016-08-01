a56358c81aa3b31bf239cd648d1b7cb09fa9cb97
Liki Du <Liki Du>
2016-08-01 13:59:35 +0800

    working on hub page

--- client/containers/Hub/index.js
+++ client/containers/Hub/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Hub = () => (
-  <p>Hub</p>
+  <div className="hub">
+    <h2>Get started with Raspberry Pi 2/3</h2>
+    <h5>Configure, build and deploy your IoT app, within 60 minutes.</h5>
+    <div className="columns is-multiline">
+      <div className="column is-4">1</div>
+      <div className="column is-4">2</div>
+      <div className="column is-4">3</div>
+      <div className="column is-4">4</div>
+      <div className="column is-4">5</div>
+    </div>
+  </div>
 );
 
 export default Hub;
